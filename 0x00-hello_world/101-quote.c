46fcb1f8788b885a59d31b57c7c5bbafa1267b3d
ibtissame <ibtissameelidrissiofficial96@gmail.com>
2023-03-20 09:22:20 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <unistd.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
- * main - prints "and that piece of art is useful" - Dora korpar, 2015-10-19",
- * followed by anewline, to standard error.
- *Return: 1 (success)
+ * main - Entry point
+ *Return: Always 1(success)
  */
-
-
 int main(void)
+
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	write(STOOUT_FILENO, "and that piece of art is useful
+			\" .  Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
 }
