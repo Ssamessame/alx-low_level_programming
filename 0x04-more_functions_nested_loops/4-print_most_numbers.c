c326b5752eae382aa3c43b8b16ec28b5ae59758e
ibtissame <ibtissameelidrissiofficial96@gmail.com>
2023-03-23 04:18:45 -0700

    more num

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,29 @@
 #include "main.h"
 
 /**
- * print_ most_numbers - print num except 2 and 4
- *
- * return: void
+ * print_ most_numbers - print numbers since 0 up to 9
+ * Description: print the numbers excluding 2 and 4
+ * Return: the numbers since 0 up to 9
  */
 
 void print_most_numbers(void)
 {
-	char c;
+	int c = 0;
 
-	for (c = '0'; c <= '9'; c++)
+	for (; c <= '9'; c++)
 	{
-		if (!(c == '2' || c =='4'))
-			_putchar(c);
+	if (c == 2 || c == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(c + '0');
+	}
 	}
 	_putchar('\n');
 }
+
+
+
+
