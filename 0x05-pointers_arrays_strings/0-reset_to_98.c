e8e5550e3f9d4d887b63cd39d9e11c2102658a35
ibtissame <ibtissameelidrissiofficial96@gmail.com>
2023-03-27 16:09:18 -0700

    1 swap

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
- * rest_to_98 - Takes a pointer to an int and updates
- * the value it points to to 98
- *
+ * rest_to_98 - a function that  Takes a pointer to an int and updates
+ * and updates the value it points to to 98
  * @n: An integer input pointer
- *
  * Return : nothing
  */
 void reset_to_98(int *n)
